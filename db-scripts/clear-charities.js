00cf6b4fc0803041f8d49a9976a89671c424481a
DESKTOP-74U8AQU\PropertyOf_Taliban <Oleg.Karpenkov@dataart.com>
2018-06-30 12:59:57 +0300

    no message

--- db-scripts/clear-charities.js
+++ db-scripts/clear-charities.js
@@ -2,11 +2,20 @@ let
   db = require('./db-connection'),
   mongoose = require('mongoose');
 
-db.connect();
-
-mongoose.connection.on('open', ref => {
-
-  console.log(mongoose.connection.db);
-
-});
+db.getCollection('registered-users')
+  .then(collection => {
+    collection.findOne({_id: mongoose.Types.ObjectId('5b334a0de12b7502685dc933')})
+      .then(res => {
+        console.log(res);
+        db.disconnect();
+      })
+      .then(err => {
+        console.log(err);
+        db.disconnect();
+      });
+  })
+  .catch(err => {
+    console.log(err);
+    db.disconnect();
+  });
 
