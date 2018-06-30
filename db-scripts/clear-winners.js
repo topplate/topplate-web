845cd09b9823d784675176763b8b8b9fd9965bf1
DESKTOP-74U8AQU\PropertyOf_Taliban <Oleg.Karpenkov@dataart.com>
2018-06-30 06:34:48 +0300

    no message

--- db-scripts/clear-winners.js
+++ db-scripts/clear-winners.js
@@ -4,8 +4,15 @@ let
 
 db.getCollection('winners')
   .then(collection => {
-    collection.remove({})
+
+    collection.updateMany(
+      {},
+      {$set: {week: '2018_1_1'}}
+    )
       .then(res => console.log(res))
       .catch(err => console.log(err));
+
+
+
   })
   .catch(err => console.log(err));
