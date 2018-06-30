b106b1aeca78a06193fe2ac8978de28ef6d5fc34
DESKTOP-74U8AQU\PropertyOf_Taliban <Oleg.Karpenkov@dataart.com>
2018-06-30 13:12:21 +0300

    no message

--- db-scripts/clear-charities.js
+++ db-scripts/clear-charities.js
@@ -2,25 +2,44 @@ let
   db = require('./db-connection'),
   mongoose = require('mongoose');
 
-db.getCollection('registered-users')
+
+db.getCollection('charities')
   .then(collection => {
-    collection.findOne(
-      {_id: mongoose.Types.ObjectId('5b30db87ff3a0d51b2fb7d40')},
-      // {$set: { charityVotes: {}}}
-    )
-    .then(res => {
-      console.log(res);
-      db.disconnect();
-    })
-    .catch(err => {
-      console.log(err);
-      db.disconnect();
-    });
+
+    collection.findOne({_id: mongoose.Types.ObjectId('5af99a7738bc0e179ea03d12')})
+      .then(res => {
+        console.log(res);
+        db.disconnect();
+      })
+      .catch(err => {
+        console.log(err);
+        db.disconnect();
+      });
   })
   .catch(err => {
     console.log(err);
     db.disconnect();
   });
+
+// db.getCollection('registered-users')
+//   .then(collection => {
+//     collection.findOne(
+//       {_id: mongoose.Types.ObjectId('5b30db87ff3a0d51b2fb7d40')},
+//       // {$set: { charityVotes: {}}}
+//     )
+//     .then(res => {
+//       console.log(res);
+//       db.disconnect();
+//     })
+//     .catch(err => {
+//       console.log(err);
+//       db.disconnect();
+//     });
+//   })
+//   .catch(err => {
+//     console.log(err);
+//     db.disconnect();
+//   });
 
 //5af99a7738bc0e179ea03d12
 
